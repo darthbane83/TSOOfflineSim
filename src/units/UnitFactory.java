c9ad229bc34a52d561b6799260468f924f639f7e
Vladimir <wowa-ovt@web.de>
2017-05-15 10:18:16 +0200

    added standard units

--- src/units/UnitFactory.java
+++ src/units/UnitFactory.java
@@ -35,6 +35,38 @@ public class UnitFactory {
         return new Unit("PlayerR",40,15,30,0.8,2,1,false,false,remainingUnits);
     }
 
+    public Unit createPlayerM(int remainingUnits){
+        return new Unit("PlayerM",60,20,40,0.8,2,1.1,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerS(int remainingUnits){
+        return new Unit("PlayerS",90,20,40,0.85,2,1.2,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerES(int remainingUnits){
+        return new Unit("PlayerES",120,20,40,0.9,2,1.3,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerC(int remainingUnits){
+        return new Unit("PlayerC",5,5,10,0.8,1,2,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerB(int remainingUnits){
+        return new Unit("PlayerB",10,20,40,0.8,2,2.1,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerLB(int remainingUnits){
+        return new Unit("PlayerLB",10,30,60,0.8,2,2.2,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerXB(int remainingUnits){
+        return new Unit("PlayerXB",10,45,90,0.8,2,2.3,false,false,remainingUnits);
+    }
+
+    public Unit createPlayerCN(int remainingUnits){
+        return new Unit("PlayerCN",60,60,120,0.9,3,2.4,false,false,remainingUnits);
+    }
+
     //Bandits
 
     //cultists
